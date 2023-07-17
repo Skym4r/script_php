af0628c0c30168a4e2d410d3d83cfa7b3da2e8c8
Dmitry <75675320+Skym4r@users.noreply.github.com>
2023-07-17 10:18:21 +0300

    Update rezerv_tovara_retailCRM.php

--- rezerv_tovara_retailCRM.php
+++ rezerv_tovara_retailCRM.php
@@ -1,181 +1,97 @@
 <?php 
-$apiUrl = 'url_retailCRM';
-$apiKey = 'API_retailCRM';
+/*Скрипт нужен для того чтобы сделать бронь и резервирование товаров, 
+если остатков данного товара нет на складе, который выбран в заказе, 
+то товар нужен бронировать в тот склад где больше всего остатков 
+*/
+$apiUrl = 'utl_ratilCRM';
+$apiKey = 'API_TOKEN_ratilCRM';
+$id=$_GET['id'];//получаем гет парметр с запроса 
 
-$id=$id_GET['id'];
-$order=ordersGet($apiUrl, $apiKey, $id)['orders'][0];
-$site=$order['site'];
-$shipmentStore = $order['shipmentStore'];
-$n=0;
-$items=$order['items'];
-foreach($items as $item) {
-    var_dump($item['prices']);
-    var_dump($item['priceType']['code']);
-    $stores = storeInventories($apiUrl, $apiKey, $item['offer']['xmlId'],$item['offer']['externalId']);
+$order=ordersGet($apiUrl, $apiKey, $id)['orders'][0];#Получаем информацию о заказе
+$shipmentStore = $order['shipmentStore'];#склад отгрузки
+$items=$order['items'];#массив товаров
+
+foreach($items as $item) 
+{
+    $id=$item['id'];#id товара
+    $xmlid=$item['offer']['xmlId'];#xmlId товара
+    $externalId=$item['offer']['externalId'];#externalId товара
+    $price=$item['prices']['0']['price'];#цена товара
+    $quantity=$item['prices']['0']['quantity'];#колличество товара в зказе товара
+    $store=stores($apiUrl, $apiKey, $xmlid, $id, $shipmentStore, $externalId, $price, $quantity);
+}
+
+function stores($apiUrl, $apiKey, $xmlid, $id, $shipmentStore, $externalId, $price, $quantity)
+{
+    $stores = storeInventories($apiUrl, $apiKey, $xmlid, $externalId);#ищем по определенному фильтру товар на складах
     $stores=$stores['offers'];
     foreach ($stores as $elementOffer) 
     {
         foreach ($elementOffer['stores'] as $store)
         {
-            if ($store['store'] == $shipmentStore)
+            if ($store['store'] == $shipmentStore)//смотрим колличесво остатков на складе из заказа 
                 {
-                $offersQuantity[$item['offer']['xmlId']] = $store['quantity'];
-                $s+=$store['quantity'];
+                $s=$store['quantity'];
+                }
+            if ($store['store'] == 'Cклад1')//смотрим колличесво остатков на складе1
+                {
+                $s1=$store['quantity'];
+                }
+            if ($store['store'] == 'Cклад2')//смотрим колличесво остатков на складе2
+                {
+                $s2=$store['quantity'];
+                }
+            if ($store['store'] == 'Cклад3')//смотрим колличесво остатков на складе3
+                {
+                $s3=$store['quantity'];
+                }
+            if ($store['store'] == 'Cклад4')//смотрим колличесво остатков на складе4  
+                {
+                $s4=$store['quantity'];
+                }
+            if ($store['store'] == 'Cклад5')//смотрим колличесво остатков на складе5
+                {
+                $s5=$store['quantity'];
+                }
+            if ($store['store'] == 'Cклад6')//мотрим колличесво остатков на складе6 
+                {
+                $s6=$store['quantity'];
                 }
         }       
     }
-    if (($s > 0)) 
-        {
-            $newOrder[$n] = ['id' => $item['id'],'status' => 'in-reserve', 'price'=>$item['prices'][0]['price'], 'priceType'=>['code'=>$item['priceType']['code']]];
-        }
- 
-    if ($s == 0) 
+    //Сравниваем остатки на складах и выбираем самый наибольший склад
+    if (($s >= $quantity)and($s>=$s1)and($s>=$s2)and($s>=$s3)and($s>=$s4)and($s>=$s5)and($s>=$s6))
     {
-        foreach ($stores as $elementOffer) 
-            {
-                foreach ($elementOffer['stores'] as $store)
-                    {
-                        if ($store['store'] == 'SklP')
-                            {
-                                $offersQuantity[$item['offer']['xmlId']] = $store['quantity'];
-                                $s1+=$store['quantity'];
-                            }
-                    }       
-            }
-
-        if($s1>0)
-            {
-               $newOrder[$n] = ['id' => $item['id'],'status' => 'est-na-sklade-a', 'price'=>$item['prices'][0]['price'], 'priceType'=>['code'=>$item['priceType']['code']]];
-            }
-            
-        if($s1==0)
-            {
-            foreach ($stores as $elementOffer) 
-                {
-                    foreach ($elementOffer['stores'] as $store)
-                        {
-                        if ($store['store'] == 'gag')
-                            {
-                                $offersQuantity[$item['offer']['xmlId']] = $store['quantity'];
-                                $s2+=$store['quantity'];
-                            }
-                        }       
-                }
-                //var_dump($s2);
-                if($s2>0)
-                {
-                    $newOrder[$n] = ['id' => $item['id'],'status' => 'est_na_skalde_gagar', 'price'=>$item['prices'][$n]['price'], 'priceType'=>['code'=>$item['priceType']['code']]];      
-                    
-                }
-                if($s2==0)
-                {
-                    foreach ($stores as $elementOffer) 
-                        {
-                            foreach ($elementOffer['stores'] as $store)
-                            {
-                                if ($store['store'] == 'len')
-                                    {
-                                    $offersQuantity[$item['offer']['xmlId']] = $store['quantity'];
-                                    $s3+=$store['quantity'];
-                                    }
-                            }       
-                        }
-                    if($s3>0)
-                    {
-                        $newOrder[$n] = ['id' => $item['id'],'status' => 'lenina', 'price'=>$item['prices'][$n]['price'], 'priceType'=>['code'=>$item['priceType']['code']]]; 
-                        }
-                    if($s3==0)
-                    {
-                        foreach ($stores as $elementOffer) 
-                        {
-                                foreach ($elementOffer['stores'] as $store)
-                                {
-                                
-                                    if ($store['store'] == 'bek')
-                                    {
-                                    $offersQuantity[$item['offer']['xmlId']] = $store['quantity'];
-                                    $s4+=$store['quantity'];
-                                    }
-                                }       
-                        }
-                        if($s4>0)
-                        {                          
-                           $newOrder[$n] = ['id' => $item['id'],'status' => 'bek', 'price'=>$item['prices'][$n]['price'], 'priceType'=>['code'=>$item['priceType']['code']]]; 
-                        }
-                        if($s4==0)
-                        {
-                            foreach ($elementOffer['stores'] as $store)
-                                {
-                                if ($store['store'] == 'dze')
-                                    {
-                                    $offersQuantity[$item['offer']['xmlId']] = $store['quantity'];
-                                    $s5+=$store['quantity'];
-                                    }
-                                }
-                            if($s5>0)
-                                {                
-                                    $newOrder[$n] = ['id' => $item['id'],'status' => 'dzerzh', 'price'=>$item['prices'][$n]['price'], 'priceType'=>['code'=>$item['priceType']['code']]]; 
-                                } 
-                            if($s5==0)
-                            {
-                                    foreach ($elementOffer['stores'] as $store)
-                                        {
-                                        if ($store['store'] == 'dru')
-                                            {
-                                                $offersQuantity[$item['offer']['xmlId']] = $store['quantity'];
-                                                $s6+=$store['quantity'];
-                                            }
-                                        }
-                                    if($s6>0)
-                                        {
-                                            $newOrder[$n] = ['id' => $item['id'],'status' => 'druzh', 'price'=>$item['prices'][$n]['price'], 'priceType'=>['code'=>$item['priceType']['code']]]; 
-                                        } 
-                               
-                                
-                            }    
-                        }
-                    }
-                }
-            }
-
-            
+        $pack=packs($apiUrl, $apiKey, $id, $shipmentStore, $price, $quantity);
     }
-    $n++;       
-    unset($s);
-    unset($s1);
-    unset($s2);
-    unset($s3);
-    unset($s4);
-    unset($s5);
-    unset($s6);
-    
-
-}
-$ord=['items'=>$newOrder,'id'=>$id];
-$response=ordersEdit( $apiUrl, $apiKey, $ord, $id, $site);
-
-
-
-function ordersGet(string $apiUrl, string $apiKey, int $id): array
-{
-$url = "$apiUrl/api/v5/orders?apiKey=$apiKey";
-$request = http_build_query([
-'ids' => [$id]
-]);
- $curl = curl_init();
-curl_setopt_array($curl, [
-CURLOPT_URL => $url,
-CURLOPT_RETURNTRANSFER => true,
-CURLOPT_CUSTOMREQUEST => 'GET',
-CURLOPT_POSTFIELDS => $request,
-]);
-$response = curl_exec($curl);
-curl_close($curl);
-return json_decode($response, true);
+    if (($s1 >= $quantity)and($s1>=$s)and($s1>=$s2)and($s1>=$s3)and($s1>=$s4)and($s1>=$s5)and($s1>=$s6))
+    {
+       $pack=packs($apiUrl, $apiKey, $id, 'Cклад1', $price, $quantity);
+    }
+    if (($s2 >= $quantity)and($s2>=$s)and($s2>=$s1)and($s2>=$s3)and($s2>=$s4)and($s2>=$s5)and($s2>=$s6))
+    {
+        $pack=packs($apiUrl, $apiKey, $id, 'Cклад2', $price, $quantity);
+    }
+    if (($s3 >= $quantity)and($s3>=$s)and($s3>=$s1)and($s3>=$s2)and($s3>=$s4)and($s3>=$s5)and($s3>=$s6))
+    {
+        $pack=packs($apiUrl, $apiKey, $id, 'Cклад3', $price, $quantity);
+    }
+    if (($s4 >= $quantity)and($s4>=$s)and($s4>=$s1)and($s4>=$s2)and($s4>=$s3)and($s4>=$s5)and($s4>=$s6))
+    {
+        $pack=packs($apiUrl, $apiKey, $id, 'Cклад4', $price, $quantity);
+    }
+    if (($s5 >= $quantity)and($s5>=$s)and($s5>=$s1)and($s5>=$s2)and($s5>=$s3)and($s5>=$s4)and($s5>=$s6))
+    {
+        $pack=packs($apiUrl, $apiKey, $id, 'Cклад5', $price, $quantity);
+    }
+    if (($s6 >= $quantity)and($s6>=$s)and($s6>=$s1)and($s6>=$s2)and($s6>=$s3)and($s6>=$s4)and($s6>=$s5))
+    {
+        $pack=packs($apiUrl, $apiKey, $id, 'Cклад6', $price, $quantity);
+    }
 }
 
 
-function storeInventories(string $apiUrl, string $apiKey, string $xmlId, string $externalId): array
+function storeInventories($apiUrl, $apiKey, $xmlId, $externalId)
 {
 $url = "$apiUrl/api/v5/store/inventories?apiKey=$apiKey";
 $request = http_build_query([
@@ -196,14 +112,39 @@ return json_decode($response, true);
 }
 
 
-function ordersEdit(string $apiUrl, string $apiKey, array $order, int $id,  string $site)
+
+function ordersGet($apiUrl, $apiKey, $id)
 {
-$url = "$apiUrl/api/v5/orders/$id/edit?apiKey=$apiKey";
+$url = "$apiUrl/api/v5/orders?apiKey=$apiKey";
 $request = http_build_query([
-'site' => $site,
-'order' =>  json_encode($order),
+'ids' => [$id]
 ]);
  $curl = curl_init();
+curl_setopt_array($curl, [
+CURLOPT_URL => $url,
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_CUSTOMREQUEST => 'GET',
+CURLOPT_POSTFIELDS => $request,
+]);
+$response = curl_exec($curl);
+curl_close($curl);
+return json_decode($response, true);
+}
+
+
+
+function packs($apiUrl, $apiKey, $id, $store, $price, $quantity)
+{
+$url = "$apiUrl/api/v5/orders/packs/create?apiKey=$apiKey";
+$request = http_build_query([
+'pack'=>json_encode([
+    'quantity' => $quantity,
+    'store' => $store,
+    'purchasePrice'=>$initialPrice,
+    'shipmentDate' => date('Y-m-d'),
+    'itemId'=>$id])
+]);
+$curl = curl_init();
 curl_setopt_array($curl, [
 CURLOPT_URL => $url,
 CURLOPT_RETURNTRANSFER => true,
@@ -214,4 +155,5 @@ $response = curl_exec($curl);
 curl_close($curl);
 return json_decode($response, true);
 }
+          
 ?>
